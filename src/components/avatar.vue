33071383a0b12c1ae7f1f3015402635ceddd4f2c
chentao <1161735119@qq.com>
2018-04-18 11:39:04 +0800

    头像avatar组件

--- src/components/avatar.vue
+++ src/components/avatar.vue
@@ -1,9 +1,136 @@
 <template>
-    <div>
-    </div>
+    <span :class="classes">
+        <img :src="src" v-if="src">
+        <span ref="children" :class="[prefixCls + '-string']" :style="childrenStyle" v-else><slot></slot></span>
+    </span>
 </template>
 <script>
-  export default {}
+  const prefixCls = 'ivu-avatar';
+  export default {
+    name: 'Avatar',
+    props: {
+      shape: {
+        default: 'circle'
+      },
+      size: {
+        default: 'default'
+      },
+      src: {
+        type: String
+      },
+      icon: {
+        type: String
+      }
+    },
+    data () {
+      return {
+        prefixCls: prefixCls,
+        scale: 1,
+        childrenWidth: 0,
+        isSlotShow: false
+      };
+    },
+    computed: {
+      classes () {
+        return [
+          `${prefixCls}`,
+          `${prefixCls}-${this.shape}`,
+          `${prefixCls}-${this.size}`,
+          {
+            [`${prefixCls}-image`]: !!this.src,
+            [`${prefixCls}-icon`]: !!this.icon
+          }
+        ];
+      },
+      childrenStyle () {
+        let style = {};
+        if (this.isSlotShow) {
+          style = {
+            msTransform: `scale(${this.scale})`,
+            WebkitTransform: `scale(${this.scale})`,
+            transform: `scale(${this.scale})`,
+            position: 'absolute',
+            display: 'inline-block',
+            left: `calc(50% - ${Math.round(this.childrenWidth / 2)}px)`
+          };
+        }
+        return style;
+      }
+    },
+    methods: {
+      setScale () {
+        this.isSlotShow = !this.src && !this.icon;
+        if (this.$refs.children) {
+          // set children width again to make slot centered
+          this.childrenWidth = this.$refs.children.offsetWidth;
+          const avatarWidth = this.$el.getBoundingClientRect().width;
+          // add 4px gap for each side to get better performance
+          if (avatarWidth - 8 < this.childrenWidth) {
+            this.scale = (avatarWidth - 8) / this.childrenWidth;
+          } else {
+            this.scale = 1;
+          }
+        }
+      }
+    },
+    mounted () {
+      this.setScale();
+    },
+    updated () {
+      this.setScale();
+    }
+  };
 </script>
-<style>
+<style scoped>
+  .ivu-avatar {
+    display: inline-block;
+    text-align: center;
+    background: #ccc;
+    color: #fff;
+    white-space: nowrap;
+    position: relative;
+    overflow: hidden;
+    vertical-align: middle;
+    width: 32px;
+    height: 32px;
+    line-height: 32px;
+    border-radius: 16px;
+  }
+  .ivu-avatar>img {
+    width: 100%;
+    height: 100%;
+  }
+  .ivu-avatar-square {
+    border-radius: 4px;
+  }
+  .ivu-avatar .ivu-avatar-string{
+    font-size: 18px;
+  }
+  .ivu-avatar-large {
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    border-radius: 20px;
+  }
+  .ivu-avatar-large.ivu-avatar-square {
+    border-radius: 4px;
+  }
+  .ivu-avatar-large .ivu-avatar-string{
+    font-size: 24px;
+  }
+  .ivu-avatar-large.ivu-avatar-icon {
+    font-size: 24px;
+  }
+  .ivu-avatar-small {
+    width: 24px;
+    height: 24px;
+    line-height: 24px;
+    border-radius: 12px;
+  }
+  .ivu-avatar-small .ivu-avatar-string{
+    font-size: 14px;
+  }
+  .ivu-avatar.ivu-avatar-icon {
+    font-size: 18px;
+  }
 </style>
--- src/views/basemodule/avatar.vue
+++ src/views/basemodule/avatar.vue
@@ -7,30 +7,211 @@
     <div class="wrap">
       <el-collapse v-model="activeNames" @change="handleChange">
         <el-collapse-item title="组件演示" name="1">
+          <div class="mt-25">
+            <label class="left-label">基础用法:</label>
+            <avatar size="large"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar size="small"><i class="ivu-icon ivu-icon-person"></i></avatar>
 
+            <avatar size="large" shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar  shape="square" ><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar size="small"  shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+          </div>
+          <div class="mt-25">
+            <label class="left-label">类型:</label>
+            <avatar :src=src></avatar>
+            <avatar><i class="ivu-icon ivu-icon-person"></i></avatar>
+          </div>
+          <div class="mt-25">
+            <label class="left-label">带徽标的头像:</label>
+            <badge  count="1">
+              <avatar shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            </badge>
+          </div>
+          <div class="mt-25 mb-20">
+            <label class="left-label">自动调整字符大小:</label>
+            <avatar :style="{background: color}">{{ user }}</avatar>
+            <button size="small" @click="handleChangeA" class="ibutton">Change</button>
+          </div>
         </el-collapse-item>
         <el-collapse-item title="使用说明" name="2">
-          示例代码：  uiScript.multiOverflowhidden(id,rows,text);
-          参数类型：  id为指定多行省略的id或类名，rows指定第几行省略，text为文本内容
-        </el-collapse-item>
+          示例代码：
+          <div class="code">
+            基础用法： {{code1}}
 
+          </div>
+          <div class="code">
+            类型：  {{code2}}
+          </div>
+          <div class="code">
+            带徽标的头像：  {{code3}}
+          </div>
+          <div class="code">
+            自动调整字符大小：  {{code4}}
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="API" name="3">
+          <table>
+            <thead>
+            <th>属性</th>
+            <th>说明</th>
+            <th>类型</th>
+            <th>默认值</th>
+            </thead>
+            <tbody>
+            <tr>
+              <td>shape</td>
+              <td>指定头像的形状，可选值为 circle、square</td>
+              <td>String</td>
+              <td>circle</td>
+            </tr>
+            <tr>
+              <td>size</td>
+              <td>设置头像的大小，可选值为 large、small、default</td>
+              <td>String</td>
+              <td>default</td>
+            </tr>
+            <tr>
+              <td>src</td>
+              <td>图片类头像的资源地址</td>
+              <td>String</td>
+              <td>-</td>
+            </tr>
+            </tbody>
+          </table>
+        </el-collapse-item>
       </el-collapse>
     </div>
   </div>
 </template>
 <script>
+  import avatar from '../../components/avatar.vue'
+  import badge from '../../components/badge.vue'
+
+  const UserList = ['U', 'Lucy', 'Tom', 'Edward'];
+  const ColorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
+
+
   export default {
     data(){
       return{
         activeNames:['1','2'],
+        user: UserList[0],
+        color: ColorList[0],
+        src:'../../../static/images/lockon.jpg',
+        code1:`
+            <avatar size="large"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar size="small"><i class="ivu-icon ivu-icon-person"></i></avatar>
+
+            <avatar size="large" shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar  shape="square" ><i class="ivu-icon ivu-icon-person"></i></avatar>
+            <avatar size="small"  shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+        `,
+        code2:`
+            <avatar :src=src></avatar>
+            <avatar><i class="ivu-icon ivu-icon-person"></i></avatar>
+        `,
+        code3:`
+           <badge  count="1">
+              <avatar shape="square"><i class="ivu-icon ivu-icon-person"></i></avatar>
+            </badge>
+        `,
+        code4:`
+          <avatar :style="{background: color}">{{ user }}</avatar>
+        `
       }
+    },
+    components:{
+      avatar,badge
     },
     methods:{
       handleChange(val){
         console.log(val);
       },
+      handleChangeA () {
+        const index = UserList.indexOf(this.user);
+        this.user = index < UserList.length - 1 ? UserList[index + 1] : UserList[0];
+        this.color = index < ColorList.length - 1 ? ColorList[index + 1] : ColorList[0];
+      }
     },
   }
 </script>
 <style>
+  .mt-25{
+    margin-top:25px;
+  }
+  .mb-20{
+    margin-bottom:20px;
+  }
+  .left-label{
+    padding:0 10px;
+    width:80px;
+    display: inline-block;
+  }
+  table{
+    font-family: Consolas,Menlo,Courier,monospace;
+    font-size: 12px;
+    border-collapse: collapse;
+    border-spacing: 0;
+    empty-cells: show;
+    border: 1px solid #e9e9e9;
+    width: 100%;
+    margin-bottom: 24px;
+  }
+  table th {
+    border: 1px solid #e9e9e9;
+    padding: 8px 16px;
+    text-align: left;
+    background: #f7f7f7;
+    white-space: nowrap;
+    color: #5c6b77;
+    font-weight: 600;
+  }
+  table td, table th {
+    border: 1px solid #e9e9e9;
+    padding: 8px 16px;
+    text-align: left;
+  }
+  .ibutton{
+    display: inline-block;
+    margin-bottom: 0;
+    font-weight: 200;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    white-space: nowrap;
+    line-height: 1.2;
+    user-select: none;
+    outline: none;
+    padding: 6px 15px;
+    font-size: 12px;
+    border-radius: 4px;
+    color: #495060;
+    background-color: #f7f7f7;
+    border:1px solid #dddee1;
+  }
+  .ivu-icon {
+    display: inline-block;
+    font-family: Ionicons;
+    speak: none;
+    font-style: normal;
+    font-weight: 400;
+    font-variant: normal;
+    text-transform: none;
+    text-rendering: auto;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  .ivu-icon-person:before {
+    content: "\2640";
+  }
+  .code{
+    border-radius: 3px;
+    background-color:#f7f7f7;
+    color: inherit;
+    padding:5px 10px;
+
+  }
 </style>
