e6f858e26bb15e059a7d820e12ee01ef15ea8fc4
southui <south_ui@hotmail.com>
2018-04-23 16:28:33 +0800

    ...

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <nav>
+      <ul>
+        <li><router-link to="/input">input</router-link></li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components:{
+
+  }
+}
+</script>
